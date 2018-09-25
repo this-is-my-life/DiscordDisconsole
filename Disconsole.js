2ef2c355913c6bf95355e616fce210b8f0f22bd4
PMH <kok4575@gmail.com>
2018-09-25 22:52:51 +0900

    [09/25] Command Book Add

--- Disconsole.js
+++ Disconsole.js
@@ -35,7 +35,33 @@ disc.on('message', input => {
 	if (`${input.author.id}` === `${disc.user.id}`) return;
 	console.log(`${input.author.username.toString()} (${input.author.id.toString()})> ${input.content.toString()}`); // input Logging
 
+
 // Commands....................
+
+	if (i === ">./help.*") {
+		console.log("Load Command Book...")
+		let CommandBook = new API.RichEmbed()
+		.setTitle(`Command Book`)
+		.setURL("https://github.com/PMHStudio/DiscordDisconsole/")
+		.setDescription(`Welcome to Disconsole!`)
+		.setColor("#7289DA")
+		.addBlankField()
+		.addField("Batch File", "```cmd\n>echo Like This```")
+		.addField("C Language", "```c\n>printf(\"Like This\");```")
+		.addField("C# Language", "```cs\n>Console.WriteLine(\"Like This\");```")
+		.addField("C++ Language", "```cpp\nstd::cout << \"Like This\" << std::endl;```")
+		.addField("Arduino C Language", "```c\n>Serial.println(\"Like This\");```")
+		.addField("D Language", "```d\n>writeln(\"Like This\");```")
+		.addField("Lua", "```lua\n>print(\"Like This\");```")
+		.addField("Python", "```py\n>print(\"Like This\");```")
+		.addField("JavaScript", "```js\n>console.log(\"Like This\");```")
+		.addField("Basic", "```basic\n>PRINT \"Like This\"```")
+		.addField("Cobol", "```>DISPLAY \"Like This\"```")
+		.addField("FORTRAN", "```fortran\n>Print *, \"Like This\"```")
+		.addField("Go", "```go\n>fmt.Printf(\"Like This\");```")
+		.setFooter("Discord + Console = Disconsole.", disc.user.displayAvatarURL);
+		input.channel.send(CommandBook);
+	} else
 
 	if (i === ">echo") {
 		console.log(`-----------Convert: ${input.id}----------`);
@@ -107,7 +133,6 @@ disc.on('message', input => {
 		let langArr = langAr[1].split("\"");
 		buffer = langArr[0];
 		console.log(`Check String: ${buffer}`);
-		if (!langAr[2]) return input.channel.send("ERROR: Endless! (Disconsole Error Code(DcEC): 5x01)");
 		console.log("-----------------------------------------------");
 		console.log(`${disc.user.username.toString()} (${disc.user.id.toString()})> ${buffer}`);
 		input.channel.send(buffer);
@@ -146,7 +171,6 @@ disc.on('message', input => {
 		let langArr = langAr[1].split("\"");
 		buffer = langArr[0];
 		console.log(`Check String: ${buffer}`);
-		if (!langAr[2]) return input.channel.send("ERROR: Endless! (Disconsole Error Code(DcEC): 5x01)");
 		console.log("-----------------------------------------------");
 		console.log(`${disc.user.username.toString()} (${disc.user.id.toString()})> ${buffer}`);
 		input.channel.send(buffer);
@@ -172,7 +196,6 @@ disc.on('message', input => {
 		let langArr = langAr[1].split("\"");
 		buffer = langArr[0];
 		console.log(`Check String: ${buffer}`);
-		if (!langAr[2]) return input.channel.send("ERROR: Endless! (Disconsole Error Code(DcEC): 5x01)");
 		console.log("-----------------------------------------------");
 		console.log(`${disc.user.username.toString()} (${disc.user.id.toString()})> ${buffer}`);
 		input.channel.send(buffer);
@@ -182,19 +205,6 @@ disc.on('message', input => {
 		console.log(`-----------Convert: ${input.id}----------`);
 		console.log("Check Language: go");
 		input.channel.send("Language: Go ----------------------------");
-		let langArr = langAr[1].split("\"");
-		buffer = langArr[0];
-		console.log(`Check String: ${buffer}`);
-		if (!langAr[2]) return input.channel.send("ERROR: Endless! (Disconsole Error Code(DcEC): 5x01)");
-		console.log("-----------------------------------------------");
-		console.log(`${disc.user.username.toString()} (${disc.user.id.toString()})> ${buffer}`);
-		input.channel.send(buffer);
-	} else
-
-	if (li === ">DISPLAY") {
-		console.log(`-----------Convert: ${input.id}----------`);
-		console.log("Check Language: cobol");
-		input.channel.send("Language: Cobol -------------------------");
 		let langArr = langAr[1].split("\"");
 		buffer = langArr[0];
 		console.log(`Check String: ${buffer}`);
